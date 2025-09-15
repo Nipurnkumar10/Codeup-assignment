d45591dd3bc43a95f6a941f608f77fe380fbaa25
Nipurn kumar <nipurnkumar295@gmail.com>
2025-09-15 15:13:56 +0530

    Program to print table of a number

---
+++ Table.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Table {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter a no: ");
+    int num = sc.nextInt();
+    
+    System.out.println("Multiplication table of " + num + ":" );
+    for(int i = 1; i<= 10; i++) {
+        System.out.println(num + " x " + i + " = " + (num * i));
+    }
+    sc.close();
+   
+}
+    
+}
